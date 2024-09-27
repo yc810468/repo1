cc26115a92ea85302f8b4e57fe82c92f5a022a59
yc810468 <chouyoonchen68@gmail.com>
2024-09-27 15:20:10 +0800

    add c.cpp

--- c.cpp
+++ c.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 int main()
 {
-    cout << "fuck world!" << endl;
+    int sum = 0;
+    for(int i=1;i<=100;i++)
+    {
+        sum+=i;
+    }
+    cout << sum << endl;
     return 0;
 }
